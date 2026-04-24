d62be9009d5b66e5a4cd8cd1adeb834bdc0f2907
AO-Alumno <AO-Alumno@duoc.acad>
2026-04-24 09:47:02 -0400

    ProductoServicio realizado

--- 2-independientes/ms-productos/ms-productos/src/main/java/com/almacen/ms_productos/service/ProductoService.java
+++ 2-independientes/ms-productos/ms-productos/src/main/java/com/almacen/ms_productos/service/ProductoService.java
@@ -11,6 +11,46 @@ import com.almacen.ms_productos.repository.ProductoRepository;
 public class ProductoService {
 
     @Autowired
-    private ProductoRepository repository;
+    private ProductoRepository productoRepository;
 
+    public Producto crear(Producto producto){
+        // Validaciones
+        return productoRepository.save(producto);
+    }
+
+    // CRUD: Listar todos los productos
+    public List<Producto> listar(){             // Lista todo
+        return productoRepository.findAll();
+    }
+
+    // CRUD: Buscar por Id (por id)
+    public Producto obtener(Long id) {
+        return productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe producto con tal id"));
+    }
+
+    // CRUD: Actualizar datos
+    public Producto actualizar(Long id, Producto productoActualizado) {
+        Producto existe = productoRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("No existe producto con tal id"));
+
+        // actualizas solo los campos necesarios
+        existe.setNombre(productoActualizado.getNombre());
+        existe.setMarca(productoActualizado.getMarca());
+        existe.setDescripcion(productoActualizado.getDescripcion());
+        existe.setIdCategoria(productoActualizado.getIdCategoria());
+        existe.setPrecioVenta(productoActualizado.getPrecioVenta());
+        existe.setUnidadMedida(productoActualizado.getUnidadMedida());
+        existe.setContenidoNeto(productoActualizado.getContenidoNeto());
+
+        return productoRepository.save(existe);
+    }
+
+    // CRUD: Eliminar
+    public void eliminar(Long id) {
+    Producto existe = productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("No existe producto con tal id"));
+
+    productoRepository.delete(existe);
+    }
 }
