35c7cbfd9b8a5b2f4afa23a4c1566576ec1b4ede
isabelrosemc <isamunocas@gmail.com>
2026-05-15 18:10:29 -0400

    ms-categoria finalizado

--- 2-independientes/ms-categoria/ms-categoria/src/main/java/com/almacen/ms_categoria/repository/CategoriaRepository.java
+++ 2-independientes/ms-categoria/ms-categoria/src/main/java/com/almacen/ms_categoria/repository/CategoriaRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-    Optional<Categoria> findByNombre(String nombre);
+    Optional<Categoria> findByNombreIgnoreCase(String nombre);
 
 }
