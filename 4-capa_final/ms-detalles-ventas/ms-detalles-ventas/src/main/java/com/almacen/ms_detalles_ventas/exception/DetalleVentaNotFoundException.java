4d73488cbb075c47f8f9a096b1efc55b5daaa6b5
isabelrosemc <isamunocas@gmail.com>
2026-05-17 16:14:21 -0400

    reubicacion ms-reportes y eliminacion de ms-detalle-ventas

--- 4-capa_final/ms-detalles-ventas/ms-detalles-ventas/src/main/java/com/almacen/ms_detalles_ventas/exception/DetalleVentaNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.almacen.ms_detalles_ventas.exception;
-
-public class DetalleVentaNotFoundException
-        extends RuntimeException {
-
-    public DetalleVentaNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
