1b9ce1a165f9320a519305cacd43c427785ad7e2
isabelrosemc <isamunocas@gmail.com>
2026-05-18 16:59:32 -0400

    proyecto finalizado 1.0

--- 4-capa_final/ms-pagos/ms-pagos/src/main/java/com/almacen/ms_pagos/controller/PagoController.java
+++ 4-capa_final/ms-pagos/ms-pagos/src/main/java/com/almacen/ms_pagos/controller/PagoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pagos")
+@RequestMapping("/api/pagos")
 @RequiredArgsConstructor
 public class PagoController {
 
